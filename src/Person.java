d5c2195c69253bca11ffd805099fdde14cf91592
sidashen <shashasi8023@163.com>
2020-02-10 21:27:15 +0800

    [shasha] feat: student class

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,17 @@
 public class Person {
+  public int id;
+  public String name;
+  public int age;
+
+  public Person (int id, String name, int age) {
+    this.id = id;
+    this.name = name;
+    this.age = age;
+  }
+
+  public String introduce (String name, int age) {
+    String description = "My name is" + name + ". I am " + age + "years old.";
+    return description;
+  }
 
 }
---
+++ src/Student.java
@@ -0,0 +1,6 @@
+public class Student extends Person{
+
+  public Student(int id, String name, int age) {
+    super(id, name, age);
+  }
+}
