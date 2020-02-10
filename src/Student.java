25d3329349021f04f2a6297962190e894f0a23d6
sidashen <shashasi8023@163.com>
2020-02-10 21:32:06 +0800

    [shasha] feat: attribute of class

--- src/Student.java
+++ src/Student.java
@@ -1,6 +1,18 @@
 public class Student extends Person{
+  public int class;
 
   public Student(int id, String name, int age) {
     super(id, name, age);
   }
+
+  public int getStudentClass() {
+    return class;
+  }
+
+  public void setStudentClass(int class) {
+    this.class = class;
+  }
+
+  
+
 }
