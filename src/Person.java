680e42b53df3b95cddc9fb12a7faece11402e01d
sidashen <shashasi8023@163.com>
2020-02-11 20:23:19 +0800

    [shasha] refactor: introduce method of Teacher

--- src/Person.java
+++ src/Person.java
@@ -38,4 +38,6 @@ public class Person {
     return description;
   }
 
+
+
 }
--- src/Teacher.java
+++ src/Teacher.java
@@ -1,27 +1,30 @@
 import java.util.ArrayList;
 
 public class Teacher extends Person {
-  private ArrayList classes;
+  private ArrayList<Class> classes;
 
-  public Teacher(int id, String name, int age, ArrayList classes) {
+  public Teacher(int id, String name, int age, ArrayList<Class> classes) {
     super(id, name, age);
     this.classes = classes;
   }
-
 
   public ArrayList getClasses() {
     return classes;
   }
 
-  public void setClasses(ArrayList classes) {
+  public void setClasses(ArrayList<Class> classes) {
     this.classes = classes;
   }
 
   public String introduce () {
     String description = "";
-    if (this.classes.size() != 0) {
+    if (classes.size() != 0) {
+      String myClasses = "";
+      for (Class klass :classes){
+        myClasses += klass;
+      }
       description = "My name is" + this.getName() + ". I am " + this.getAge() + "years old.  " +
-        "I am a Teacher. I teach Class" + this.getClasses() + ".";
+        "I am a Teacher. I teach Class" + myClasses + ".";
     } else {
       description = "My name is" + this.getName() + ". I am " + this.getAge() + "years old.  " +
         "I am a Teacher. I teach No Class.";
